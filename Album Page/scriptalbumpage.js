5c1b758c66013eb529ffea98e97fa6f4b167bb60
Ana Matei <anamatei.099@gmail.com>
2022-03-16 23:22:19 +0000

    Update

--- Album Page/scriptalbumpage.js
+++ Album Page/scriptalbumpage.js
@@ -5,59 +5,14 @@ function changeSong() {
     "Bohemian Rhapsody");
 }
 
-function changeSong1() {
-  let newImg = (document.getElementById("image-footer").src =
-    "../images/Album covers/bohemian-rapsody.png");
-  let newTitle = (document.getElementById("title-footer").innerHTML =
-    "20th Century Fox Fanfare");
-}
 
-function changeSong2() {
-  let newImg = (document.getElementById("image-footer").src =
-    "../images/Album covers/bohemian-rapsody.png");
-  let newTitle = (document.getElementById("title-footer").innerHTML =
-    "Somebody to love");
-}
 
-function changeSong3() {
-  let newImg = (document.getElementById("image-footer").src =
-    "../images/Album covers/bohemian-rapsody.png");
-  let newTitle = (document.getElementById("title-footer").innerHTML =
-    "Doing all right- Revisited");
-}
 
-function changeSong4() {
-  let newImg = (document.getElementById("image-footer").src =
-    "../images/Album covers/bohemian-rapsody.png");
-  let newTitle = (document.getElementById("title-footer").innerHTML =
-    "Keep yourself Alive - Live at Rainbow");
-}
-function changeSong5() {
-  let newImg = (document.getElementById("image-footer").src =
-    "../images/Album covers/bohemian-rapsody.png");
-  let newTitle = (document.getElementById("title-footer").innerHTML =
-    "Killer Queen");
-}
-function changeSong6() {
-  let newImg = (document.getElementById("image-footer").src =
-    "../images/Album covers/bohemian-rapsody.png");
-  let newTitle = (document.getElementById("title-footer").innerHTML =
-    "Fat Bottomed Girl-Live in Paris");
-}
-function changeSong7() {
-  let newImg = (document.getElementById("image-footer").src =
-    "../images/Album covers/bohemian-rapsody.png");
-  let newTitle = (document.getElementById("title-footer").innerHTML =
-    "Now I'm here- Live at Hammersmith Odeon");
-}
-function changeSong8() {
-  let newImg = (document.getElementById("image-footer").src =
-    "../images/Album covers/bohemian-rapsody.png");
-  let newTitle = (document.getElementById("title-footer").innerHTML =
-    "Crazy Little Thing Called Love");
-}
+
+
 
 const albumCoverElement = document.querySelector("img[alt='Album']");
+const artistCoverElement = document.querySelector("img[alt='artist']");
 const albumTitleElement = document.querySelector(".album-title");
 const albumArtistElement = document.querySelector(".artist-name");
 const tracklistElement = document.querySelector("#tracklist");
@@ -75,25 +30,25 @@ window.onload = () => {
 
 function displayAlbum(data) {
   albumArtistElement.innerText = data.artist.name;
+  artistCoverElement.src = data.artist.picture;
   albumCoverElement.src = data.cover_medium;
   albumTitleElement.innerText = data.title;
 
   console.log(data.tracks.data);
 
-  tracklistElement.innerHTML = data.tracks.data
+   tracklistElement.innerHTML = data.tracks.data
     .map(
       (track) => `
-        <div class="py-3 trackHover">
-          <a
-            href="#"
-            class="card-title trackHover px-3"
-            style="color: white"
-            >${track.title}</a
-          >
-          <small class="duration pr-3" style="color: white">
-            ${secondsToTimestamp(track.duration)}
-          </small>
-        </div>
+      <div class="col-12 mt-2 text-white" id="playlist-column">
+      <div class="container d-flex justify-content-start">
+        <span id="playlist-hash">#</span>
+        <a href="" id="playlist-song">
+          <span>${track.title}</span>
+        </a>
+      </div>
+      <span id="playlist-time"> ${secondsToTimestamp(track.duration)}</span>
+    </div>
+  </div>
       `
     )
     .join("");
@@ -106,4 +61,4 @@ function secondsToTimestamp(seconds) {
   return `${minutes < 10 ? "0" : ""}${minutes}:${
     remainingSeconds < 10 ? "0" : ""
   }${remainingSeconds}`;
-}
+} 
